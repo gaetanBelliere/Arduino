0633571488d553238d9edeac817c5fa4fd9d6f7e
Gaëtan Bellière <59686768+gaetanBelliere@users.noreply.github.com>
2020-11-22 11:03:51 +0100

    Add blink option

--- ArduinoProjects/GuirlandeLeds/wemosServer.ino
+++ ArduinoProjects/GuirlandeLeds/wemosServer.ino
@@ -1,92 +1,170 @@
-#include <Arduino.h>
-// Load Wi-Fi library
 #include <ESP8266WiFi.h>
 
 // Replace with your network credentials
-const char* SSID = "REPLACE_WITH_YOUR_SSID"; 
-const char* PASSWORD = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid     = "*";
+const char* password = "*";
 
 // Set web server port number to 80
 WiFiServer server(80);
 
-// Assign output variables to GPIO pins
-const int output = 4;
+// Decode HTTP GET value
+String redString = "0";
+String greenString = "0";
+String blueString = "0";
+String blinkString = "0";
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int pos4 = 0;
+int pos5 = 0;
+
+// Handle Blinking
+unsigned long previousMillis = 0;
+const long interval = 1000;
+int ledState = LOW; 
+
+// Variable to store the HTTP req  uest
+String header;
+
+// Red, green, and blue pins for PWM control
+const int redPin = 13;     // 13 corresponds to GPIO13
+const int greenPin = 12;   // 12 corresponds to GPIO12
+const int bluePin = 14;    // 14 corresponds to GPIO14
+
+// Setting PWM bit resolution
+const int resolution = 256;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
-  // Initialize the output and set it to LOW
-  pinMode(output, OUTPUT);
-  digitalWrite(output, LOW);
-
-  // Connect to Wi-Fi network with SSID and PASSWORD
+  
+  // configure LED PWM resolution/range and set pins to LOW
+  analogWriteRange(resolution);
+  analogWrite(redPin, 255);
+  analogWrite(greenPin, 255);
+  analogWrite(bluePin, 255);
+  
+  // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
-  Serial.println(SSID);
-  WiFi.begin(SSID, PASSWORD);
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address 
+  // Print local IP address and start web server
   Serial.println("");
-  Serial.println("WiFi connected at IP address:");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  // Start Web Server
   server.begin();
 }
 
-// Main loop
 void loop(){
-  // Create a client and listen for incoming clients
-  WiFiClient client = server.available();   
-  
-  // Do nothing if server is not available
-  if (!client) {
-     return;
+  WiFiClient client = server.available();   // Listen for incoming clients
+  //Serial.println("Nouveau tour");
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {            // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+                 
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
+            client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
+            client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>La Kaz' maudite</h1></div>");
+            client.println("</head><body><div class=\"container\"><div class=\"row\"><h2>Que la lumiere soit</h2></div>");
+            client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Changer la couleur</a> ");
+            
+            client.println("<div class=\"custom-control custom-checkbox\"><input type=\"checkbox\" class=\"custom-control-input\" id=\"blink\"><label class=\"custom-control-label\"for=\"blink\">Faire clignoter</label></div> ");
+            client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
+            client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
+            client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"c\"+document.getElementById(\"blink\").checked +\"&\";}");
+            client.println("</script></body></html>");
+            // The HTTP response ends with another blank line
+            client.println();
+            // Request sample: /?r201g32b255&
+            // Red = 201 | Green = 32 | Blue = 255
+            
+            if(header.indexOf("GET /?r") >= 0) {
+              pos1 = header.indexOf('r');
+              pos2 = header.indexOf('g');
+              pos3 = header.indexOf('b');
+              pos4 = header.indexOf('c');
+              pos5 = header.indexOf('&');
+              redString   = header.substring(pos1+1, pos2);
+              greenString = header.substring(pos2+1, pos3);
+              blueString  = header.substring(pos3+1, pos4);
+              blinkString = header.substring(pos4+1, pos5);
+              /*Serial.println(redString.toInt());
+              Serial.println(greenString.toInt());
+              Serial.println(blinkString.toInt());*/
+                
+              if (blinkString =="false"){
+                analogWrite(redPin, 255-redString.toInt());
+                analogWrite(greenPin, 255-greenString.toInt());
+                analogWrite(bluePin, 255-blueString.toInt());
+              }
+            }
+            // Break out of the while loop
+            break;
+            
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
-  
-  // Wait a client 
-  while(!client.available()){}
-  
-  // A new client is connected, get the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
-
-  int value = LOW;
-  if (request.indexOf("/LED=ON") != -1) 
-  {
-    digitalWrite(output, HIGH);
-    value = HIGH;
-  } 
-  if (request.indexOf("/LED=OFF") != -1)
-  {
-    digitalWrite(output, LOW);
-    value = LOW;
+  if (blinkString == "true"){
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      if (ledState == LOW) {
+        ledState = HIGH;
+        analogWrite(redPin, 255-redString.toInt());
+        analogWrite(greenPin, 255-greenString.toInt());
+        analogWrite(bluePin, 255-blueString.toInt());
+      }
+      else {
+        ledState = LOW;
+        analogWrite(redPin, LOW);
+        analogWrite(greenPin, LOW);
+        analogWrite(bluePin, LOW);
+      }
+    }
   }
-  
-  // Display GPIO status
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); 
-
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.print("GPIO status: "); 
-
-  if(value == HIGH) {
-    client.print("ON");  
-  } else {
-    client.print("OFF");
-  }
-
-  client.println("<br><br>");
-  client.println("Switch manually GPIO state");
-  client.println("<br><br>");
-  client.println("Turn <a href=\"/LED=ON\">ON</a><br>");
-  client.println("Turn <a href=\"/LED=OFF\">OFF</a><br>");
-  client.println("</html>");
-
-  Serial.println("");
-  
 }
